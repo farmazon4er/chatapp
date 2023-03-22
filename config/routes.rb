7c4a08c7514b2bbecc86d302e30ae8d1a0a19073
Vasilii Bulkovskii <vasilii.bulkovskii@jetruby.com>
2023-03-22 20:39:44 +0500

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
+
+  resources :rooms
+  resources :user
+  root 'rooms#index'
+end
